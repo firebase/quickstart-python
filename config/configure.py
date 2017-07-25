198f4435869e45cc7146c1d93c3a5fd635a6258c
Arthur Thompson <arthurthompson@google.com>
2017-07-25 15:45:12 -0700

    Add README
    Change-Id: I8c07d7b41fe7220379d159a099ecc19fe2eaf5eb

--- config/configure.py
+++ config/configure.py
@@ -5,7 +5,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 
 PROJECT_ID = '<PROJECT_ID>'
-BASE_URL = 'https://www.googleapis.com'
+BASE_URL = 'https://firebaseremoteconfig.googleapis.com'
 REMOTE_CONFIG_ENDPOINT = 'v1/projects/' + PROJECT_ID + '/remoteConfig'
 REMOTE_CONFIG_URL = BASE_URL + '/' + REMOTE_CONFIG_ENDPOINT
 REMOTE_CONFIG_SCOPE = 'https://www.googleapis.com/auth/firebase.remoteconfig'
@@ -21,10 +21,10 @@ def _get_access_token():
   access_token_info = credentials.get_access_token()
   return access_token_info.access_token
 
-def _fetch():
-  """Fetch the current Firebase Remote Config template from server.
+def _get():
+  """Retrieve the current Firebase Remote Config template from server.
 
-  Fetch the current Firebase Remote Config template from server and store it
+  Retrieve the current Firebase Remote Config template from server and store it
   locally.
   """
   headers = {
@@ -36,10 +36,10 @@ def _fetch():
     with open('config.json', 'w') as f:
       f.write(resp.text)
 
-    print('Fetched template has been written to config.json')
-    print('ETag from server: {}'.format(resp.headers['X-Google-API-ETag']))
+    print('Retrieved template has been written to config.json')
+    print('ETag from server: {}'.format(resp.headers['ETag']))
   else:
-    print('Unable to fetch template')
+    print('Unable to get template')
     print(resp.text)
 
 
@@ -60,7 +60,7 @@ def _publish(etag):
   resp = requests.put(REMOTE_CONFIG_URL, data=content, headers=headers)
   if resp.status_code == 200:
     print('Template has been published.')
-    print('ETag from server: {}'.format(resp.headers['X-Google-API-ETag']))
+    print('ETag from server: {}'.format(resp.headers['ETag']))
   else:
     print('Unable to publish template.')
     print(resp.text)
@@ -72,13 +72,13 @@ def main():
   parser.add_argument('--etag')
   args = parser.parse_args()
 
-  if args.action and args.action == 'fetch':
-    _fetch()
+  if args.action and args.action == 'get':
+    _get()
   elif args.action and args.action == 'publish' and args.etag:
     _publish(args.etag)
   else:
     print('''Invalid command. Please use one of the following commands:
-python configure.py --action=fetch
+python configure.py --action=get
 python configure.py --action=publish --etag=<LATEST_ETAG>''')
 
 
